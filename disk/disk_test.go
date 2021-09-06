fb56cf411f3fdb7b5135ea537d3c74059500ef95
Takaya Yamazoe <takaya.yamazoe@zozo.com>
2021-09-06 22:44:21 +0900

    fix test

--- disk/disk_test.go
+++ disk/disk_test.go
@@ -2,28 +2,28 @@ package disk_test
 
 import (
 	"testing"
-  "fmt"
+
 	"github.com/tmrekk121/relly/disk"
 )
 
-
 func TestNew(t *testing.T) {
-  dm, err := disk.Open("../testdata/testnew.txt")
-  if err != nil {
-    t.Fatalf("failed test %v", err)
-  }
-  buf := make([]byte, 1024)
-  for {
-      n, err := dm.HeapFile.Read(buf)
-      if n == 0{
-          break
-      }
-      if err != nil{
-          break
-      }
-  }
-  if string(buf) != "Test New" {
-    fmt.Println(string(buf))
-    t.Fatalf("missing text: %v", string(buf))
-  }
+	dm, err := disk.Open("../testdata/testnew.txt")
+	if err != nil {
+		t.Fatalf("failed test %v", err)
+	}
+	buf := make([]byte, 1024)
+	var str string
+	for {
+		n, err := dm.HeapFile.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			break
+		}
+		str = string(buf[:n])
+	}
+	if str != "Test New\n" {
+		t.Fatalf("missing text: %+v", str)
+	}
 }
